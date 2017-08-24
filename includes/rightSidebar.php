2e0c285849a76b36909fa1a504ce6324b4d54f67
Brad Penney <penney.brad@gmail.com>
2017-08-24 11:57:30 -0300

    Added GoogleAds

---
+++ includes/rightSidebar.php
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- rightSidebar -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6419585527167625"
+     data-ad-slot="2703875308"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
